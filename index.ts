333d164a8f9d2ad87a16277f7e10b8b4af49f92a
sujalkrsoni <sujalkrsoni@gmail.com>
2026-07-21 15:12:27 +0530

    Revert "5th commit"
    This reverts commit 3746468d9f6e0e2f3779bb175f8875931d2228f4.

--- index.ts
+++ index.ts
@@ -17,9 +17,3 @@ console.log("hello 2nd commit");
 console.log("hello 3rd commit");
 
 console.log("hello 4th commit");
-
-console.log("hello 5th commit");
-
-console.log("hello 6th commit");
-
-console.log("hello 7th commit");
